f31915736eedd0eb834349e857277af5866ba2db
imohdaffan <nogdsek@gmail.com>
2025-05-29 17:24:53 -0500

    Filter Example

--- Chapter 13/05_map_filter_reduce.py
+++ Chapter 13/05_map_filter_reduce.py
@@ -1,7 +1,16 @@
-
+# map Example
 l = [1, 2, 3, 4, 5]
 
 square = lambda x: x*x
 
 sqList = map(square, l)
 print(list(sqList))
+
+# filter Example
+def even(n):
+    if (n%2 == 0):
+        return True
+    return False
+
+onlyEven = filter(even, l)
+print(list(onlyEven))
